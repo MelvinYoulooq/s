077b2bcec052f89b00338bfe710fdb376e839128
MelvinYoulooq <85878098+MelvinYoulooq@users.noreply.github.com>
2021-09-11 07:47:07 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,36 @@
+const stripe = require('stripe')('sk_test_51JT6LfBujBcWAj0Aiu1ipwABxRwSDiHTBJ5rlhSe8yTMX0HyWGta7MAZBm6jmQbsbcpUnOuy6gJcJ41elAle4svQ00W7YXTXTh');
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+const YOUR_DOMAIN = 'http://localhost:4242';
+
+app.post('/create-checkout-session', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    customer_email: 'customer@example.com',
+    submit_type: 'donate',
+    billing_address_collection: 'auto',
+    shipping_address_collection: {
+      allowed_countries: ['US', 'CA'],
+    },
+    line_items: [
+      {
+        // TODO: replace this with the `price` of the product you want to sell
+        price: '{{PRICE_ID}}',
+        quantity: 1,
+      },
+    ],
+    payment_method_types: [
+      'card',
+      'ideal',
+      'sofort',
+    ],
+    mode: 'payment',
+    success_url: `${YOUR_DOMAIN}/success.html`,
+    cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+  });
+
+  res.redirect(303, session.url)
+});
+
+app.listen(4242, () => console.log('Running on port 4242'));
